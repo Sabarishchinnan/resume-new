a213eb9b7eef5d1d91f54e0ea7861a1f969c1f4e
Sabarishchinnan <sabarishc14@gmail.com>
2024-02-18 20:16:11 +0530

    added static file changes

--- resume/urls.py
+++ resume/urls.py
@@ -25,3 +25,7 @@ urlpatterns = [
     path('projects', include('projects.urls')),
     path('contact', include('contact.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
